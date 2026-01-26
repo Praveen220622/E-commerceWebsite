463bd55f1bc2d3de996fea50adec26297c95f007
Praveen Bolla <140931654+Praveen220622@users.noreply.github.com>
2026-01-26 20:26:28 +0530

    Update header text to 'Lorem Ipsum Is Easy'

--- src/stores/components/Header/Header.jsx
+++ src/stores/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
           />
         </div>
         <div className="Heading-container">
-          <h1 className="Heading">Lorem Ipsum</h1>
+          <h1 className="Heading">Lorem Ipsum Is Easy</h1>
         </div>
       </div>
     </>
